3222df63c92d13a9c7d12c9b3686c2a5288daa53
yimin12 <43520355+yimin12@users.noreply.github.com>
2024-11-10 00:37:15 -0500

    some sorting implementation
    sorting implementation

---
+++ java/src/template/algorithm/sort/SelectionSort.java
@@ -0,0 +1,28 @@
+package src.template.algorithm.sort;
+
+public class SelectionSort extends Sort {
+
+    Comparable[] comparables;
+
+    public SelectionSort(Comparable[] comparables) {
+        this.comparables = comparables;
+    }
+
+    public static void sort(Comparable[] comparables, int left, int right) {
+        if (comparables == null || comparables.length == 0 || left >= right) return;
+        for (int i = left; i < right; i ++) {
+            int swapIndex = i;
+            for (int j = i + 1; j <= right; j ++) {
+                if (comparables[j].compareTo(comparables[swapIndex]) < 0) {
+                    swapIndex = j;
+                }
+            }
+            swap(comparables, i, swapIndex);
+        }
+    }
+
+    @Override
+    void sort(Comparable[] comparables) {
+        SelectionSort.sort(comparables, 0, comparables.length - 1);
+    }
+}
---
+++ java/src/template/algorithm/sort/Sort.java
@@ -0,0 +1,45 @@
+package src.template.algorithm.sort;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public abstract class Sort<T extends Comparable<T>> {
+
+    abstract void sort(T[] comparables);
+
+    public static void swap(Object[] array, int i, int j) {
+        swap_temp_memory(array, i, j);
+    }
+
+    /**
+     * Swap 有三种实现方式
+     *  1. 使用临时变量 -> 没有限制
+     *  2. 加减 -> 适用于可加减的数值类型交换，注意避免溢出
+     *  3. 异或 -> 同样只适用于数字类型交换
+     * 需要特别注意：
+     *  2，3 在遍历元素进行 swap 时， swap不能与自己做交换， 结果值会变成 0，第一种则无需特别注意
+     * @param array
+     * @param i
+     * @param j
+     */
+    public static void swap_temp_memory(Object[] array, int i, int j) {
+        Object temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void swap_add_minus(Integer[] numbers, int i, int j) {
+        if (i == j) return;
+        numbers[i] = numbers[i] + numbers[j];
+        numbers[j] = numbers[i] - numbers[j];
+        numbers[i] = numbers[i] - numbers[j];
+    }
+
+    public static void swap_xor(Integer[] numbers, int i, int j) {
+        if (i == j) return;
+        numbers[i] = numbers[i] ^ numbers[j];
+        numbers[j] = numbers[i] ^ numbers[j];
+        numbers[i] = numbers[i] ^ numbers[j];
+    }
+
+}
